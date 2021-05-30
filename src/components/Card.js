da5df4b4ececd39c4f27c783841120afcaa83420
ghaniasim <ghaniasimm@gmail.com>
2021-05-30 14:31:55 +0300

    complete application

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,60 +1,89 @@
-import React, {useState} from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native'
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
 
 const Card = ({onPress}) => {
+  const [cardView, setCardView] = useState([
+    {
+      name: 'What is Cricket?',
+      url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF-OxlZiLVrLd6Ew70XteBOLFwOcVRmM_OMQ&usqp=CAU',
+      key: 1,
+    },
+    {
+      name: 'Batting',
+      url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuBD2vDbArNxqUAX-PTdcuQkltVnEJwKLDFA&usqp=CAU',
+      key: 2,
+    },
+    {
+      name: 'Bowling',
+      url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-jGYwoEX25hXsxiYBMNNAeR0LpTB2dPxeQ&usqp=CAU',
+      key: 3,
+    },
+    {
+      name: 'Fielding',
+      url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ65_Clz9niPF_4baZaFW_yMBLnvgEPpmycw&usqp=CAU',
+      key: 4,
+    },
+    {
+      name: '',
+      url: '',
+      key: 5,
+    },
+  ]);
 
-    const [cardView, setCardView] = useState([
-        { name: 'What is Cricket?', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF-OxlZiLVrLd6Ew70XteBOLFwOcVRmM_OMQ&usqp=CAU', key: 1},
-        { name: 'Batting', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuBD2vDbArNxqUAX-PTdcuQkltVnEJwKLDFA&usqp=CAU', key: 2},
-        { name: 'Bowling', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-jGYwoEX25hXsxiYBMNNAeR0LpTB2dPxeQ&usqp=CAU', key: 3},
-        { name: 'Fielding', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ65_Clz9niPF_4baZaFW_yMBLnvgEPpmycw&usqp=CAU', key: 4},
-    ])
+  let {container, cardText, card, cardImage} = styles;
 
-    let {container, cardText, card, cardImage} = styles
+  return (
+    <View style={container}>
+      <ScrollView style={card}>
+        {cardView.map(item => {
+          return (
+            <TouchableOpacity onPress={onPress} key={item.key}>
+              <Image style={cardImage} source={{uri: item.url}} />
+              <Text style={cardText}>{item.name}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
 
-    return (
-        <View style={container}>
-            <ScrollView style={card}>
-            {   
-                cardView.map((item) => {
-                    return (
-                        <TouchableOpacity onPress={onPress} key={item.key}>
-                            <Image style={cardImage} source={{uri: item.url}} />
-                            <Text style={cardText}>{item.name}</Text>
-                        </TouchableOpacity>
-                    )
-                })
-            }
-            </ScrollView>
-        </View>
-    )
-}
-
-export default Card
+export default Card;
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    cardText: {
-        fontSize: 30,
-        padding: 10
-    },
-    card: {
-        shadowOpacity: 1.0,
-        shadowOffset: {width: 3, height: 3},
-        shadowColor: 'black',
-        elevation: 3,
-        backgroundColor : "#0000",
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: '2%',
-        width: '96%',  
-    },
-    cardImage: {
-        width: '100%',
-        height: 200,
-        resizeMode: 'cover'
-    }
-})
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardText: {
+    fontSize: 30,
+    padding: 10,
+  },
+  card: {
+    shadowOpacity: 1.0,
+    shadowOffset: {width: 3, height: 3},
+    shadowColor: 'black',
+    elevation: 3,
+    backgroundColor: '#0000',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: '2%',
+    width: '96%',
+  },
+  cardImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+  },
+});
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import {StyleSheet, Text, View, ScrollView, Dimensions} from 'react-native';
-import {data} from '../data';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  Dimensions,
+  Alert,
+} from 'react-native';
 
 const {width, height} = Dimensions.get('window');
 
 const Question = () => {
+  const [questionOne, setQuestionOne] = useState(false);
+  const [questionTwo, setQuestionTwo] = useState(false);
+  const [questionThree, setQuestionThree] = useState(false);
+  const [correct, setCorrect] = useState(false);
+
+  const onPressOne = () => {
+    setQuestionOne(!questionOne);
+  };
+
+  const onPressTwo = () => {
+    setQuestionTwo(!questionTwo);
+  };
+
+  const onPressThree = () => {
+    setQuestionThree(!questionThree);
+  };
+
+  const onSubmit = () =>
+    Alert.alert(
+      'Your Result',
+      'You give 2/3 correct answers and got 150 points',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'Review', onPress: () => setCorrect(true)},
+      ],
+    );
+
   return (
     <ScrollView style={{backgroundColor: '#F5FCFF', paddingTop: 10}}>
       <View style={styles.container}>
@@ -24,8 +62,17 @@ const Question = () => {
           <View style={styles.options}>
             <Text style={styles.optionText}>Running</Text>
           </View>
-          <View style={styles.selectedOption}>
-            <Text style={styles.optionText}>Sport</Text>
+          <View
+            style={
+              questionOne && correct
+                ? styles.selectedCorrectOption
+                : questionOne
+                ? styles.selectedOption
+                : styles.options
+            }>
+            <Text style={styles.optionText} onPress={onPressOne}>
+              Sport
+            </Text>
           </View>
           <View style={styles.options}>
             <Text style={styles.optionText}>Driving</Text>
@@ -45,11 +92,20 @@ const Question = () => {
               How many overs are in one day Cricket?
             </Text>
           </View>
-          <View style={styles.selectedOption}>
+          <View style={correct ? styles.selectedCorrectOption : styles.options}>
             <Text style={styles.optionText}>50</Text>
           </View>
-          <View style={styles.options}>
-            <Text style={styles.optionText}>90</Text>
+          <View
+            style={
+              questionTwo && !correct
+                ? styles.selectedOption
+                : questionTwo
+                ? styles.selectedWrongOption
+                : styles.options
+            }>
+            <Text style={styles.optionText} onPress={onPressTwo}>
+              90
+            </Text>
           </View>
           <View style={styles.options}>
             <Text style={styles.optionText}>40</Text>
@@ -70,8 +126,17 @@ const Question = () => {
           <View style={styles.oval}>
             <Text style={styles.welcome}>What is T20 stands for?</Text>
           </View>
-          <View style={styles.selectedOption}>
-            <Text style={styles.optionText}>Twenty Twenty</Text>
+          <View
+            style={
+              questionOne && correct
+                ? styles.selectedCorrectOption
+                : questionThree
+                ? styles.selectedOption
+                : styles.options
+            }>
+            <Text style={styles.optionText} onPress={onPressThree}>
+              Twenty Twenty
+            </Text>
           </View>
           <View style={styles.options}>
             <Text style={styles.optionText}>Test</Text>
@@ -82,6 +147,19 @@ const Question = () => {
           <View style={styles.options}>
             <Text style={styles.optionText}>Three Days</Text>
           </View>
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginTop: 20,
+            marginBottom: 100,
+          }}>
+          <Button title="Submit" onPress={onSubmit} />
         </View>
       </View>
     </ScrollView>
@@ -112,6 +190,22 @@ const styles = StyleSheet.create({
     width: (width * 50) / 100,
     borderRadius: 20,
     backgroundColor: 'orange',
+  },
+  selectedCorrectOption: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5,
+    width: (width * 50) / 100,
+    borderRadius: 20,
+    backgroundColor: 'chartreuse',
+  },
+  selectedWrongOption: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5,
+    width: (width * 50) / 100,
+    borderRadius: 20,
+    backgroundColor: 'red',
   },
   optionText: {
     fontSize: 20,
